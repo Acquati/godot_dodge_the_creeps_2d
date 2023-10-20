e9e21dadac7c4ade647ab0af6a4ea0c172d8ba10
Acquati <leandroacquati@gmail.com>
2023-10-19 21:46:08 -0300

    finish heads_up_display part

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -14,11 +14,18 @@ func _process(_delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -44,6 +51,8 @@ func _on_mob_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+	
+	$HUD.update_score(score)
 
 func _on_score_timer_timeout():
 	score += 1
